fe368bec83e093d3dfb22d69221cbeb280786ebf
Saoki11111 <t.aoki11111@gmail.com>
2023-03-10 17:50:19 +0900

    完全マッチしたらトップにリダイレクト

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { MembersComponent } from './members/members.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent},
   { path: 'members', component: MembersComponent}
 ];
