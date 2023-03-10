90d2cf56ac7ce9b4c616d06b556d2d812db894fa
Saoki11111 <t.aoki11111@gmail.com>
2023-03-10 17:49:17 +0900

    routing の設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MembersComponent } from './members/members.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
   { path: 'members', component: MembersComponent}
 ];
 
