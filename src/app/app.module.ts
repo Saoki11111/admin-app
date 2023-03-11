fc00e9c706042c00c1ea74c949e47cd6200f44f0
Saoki11111 <t.aoki11111@gmail.com>
2023-03-11 11:06:57 +0900

    HttpClientInMemoryWebApiModule の有効化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service'
 
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
@@ -22,6 +24,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
     AppRoutingModule
   ],
   providers: [],
