e4c250b9017e78fc1d572f7cd0d5e293c4cf04de
Saoki11111 <t.aoki11111@gmail.com>
2023-03-13 10:54:03 +0900

    検索機能の実装

--- src/app/member-search/member-search.component.ts
+++ src/app/member-search/member-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounce, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Member } from '../member';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-member-search',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-search.component.css']
 })
 export class MemberSearchComponent implements OnInit {
+  members$: Observable<Member[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private memberService: MemberService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.members$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.memberService.searchMembers(term))
+    );
   }
 
 }
