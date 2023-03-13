6b82b517f9da35658e012e311b93fcd935d3bbf2
Saoki11111 <t.aoki11111@gmail.com>
2023-03-13 14:47:03 +0900

    コメントの追加

--- src/app/member-search/member-search.component.ts
+++ src/app/member-search/member-search.component.ts
@@ -11,7 +11,7 @@ import { MemberService } from '../member.service';
 })
 export class MemberSearchComponent implements OnInit {
   members$: Observable<Member[]>;
-  private searchTerms = new Subject<string>();
+  private searchTerms = new Subject<string>(); // Observable を継承している
 
   constructor(private memberService: MemberService) { }
 
@@ -21,8 +21,20 @@ export class MemberSearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.members$ = this.searchTerms.pipe(
+      // キーボード入力の後、300ms 待って次の実行に移る
+      // キーボードの連続入力のとき発生するイベントを間引く
       debounceTime(300),
+
+      // 直前のデータと同じ場合は処理を実行しない
+      // 同じキーワードが入力されたときむだな通信は発生しないは処理しない
       distinctUntilChanged(),
+
+      // 検索キーワードを受けるたびに、新しい Observable を返す
+      // searchTerms Observable は 検索キーワードが渡ってくる
+      // 検索キーワードを受け取り、callBack で searchMembsers メソッドにキーワードを渡し、
+      // HTTP request を実行 → Observable が返される
+      // switchMap で新しい members$ をセットする
+
       switchMap((term: string) => this.memberService.searchMembers(term))
     );
   }
